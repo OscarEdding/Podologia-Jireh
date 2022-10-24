f5a977512bc22ea4bfa25904b8817ed4694ff0e6
OscarEdding <oscaredding2848@hotmail.com>
2022-10-23 22:02:38 -0300

    navbar + carousel

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./Tailwind.css"
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import Header from './components/Header'
+import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Home from "./pages/Home"
 
 function PodologiaJireh() {
   return (
     <div className="">
-      <Header />
+      <Navbar />
+      <Home />
       < Footer />
     </div>
-  );
+  )
 }
 
 export default PodologiaJireh
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { React } from "react";
+import { React } from "react"
 
 
 const Footer = () => {
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="">
       
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
