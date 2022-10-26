6507a66aa30fd32f4df61b85c8047ef0e94ffe41
OscarEdding <oscaredding2848@hotmail.com>
2022-10-26 13:57:08 -0300

    build configure

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,6 @@
 import { React } from "react"
 
 import Logo from "../../assets/logo.png"
-import ROUTES from "../../constants/routes"
-
 
 const Footer = () => {
 
@@ -38,7 +36,7 @@ const Footer = () => {
                   <a href="https://www.instagram.com/podologiaclinicjireh/" className="hover:underline ">Instagram</a>
                 </li>
                 <li>
-                  <a href="#" className="hover:underline">Facebook</a>
+                  <a href="/#" className="hover:underline">Facebook</a>
                 </li>
               </ul>
             </div>
@@ -60,7 +58,7 @@ const Footer = () => {
           <span className="text-sm text-gray-300 sm:text-center mr-5">© 2022 <a href="https://www.instagram.com/podologiaclinicjireh/" className="hover:underline">Podología JIREH™</a>. Todos los derechos reservados.
           </span>
           <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-            <a href="#" className="text-gray-300 hover:text-blue-600">
+            <a href="/#" className="text-gray-300 hover:text-blue-600">
               <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true" data-darkreader-inline-fill=""><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd"></path></svg>
               <span className="sr-only">Facebook page</span>
             </a>
