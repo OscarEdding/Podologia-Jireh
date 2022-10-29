68e0ba12a0801d8419d0e30632c241ada12be2fe
OscarEdding <oscaredding2848@hotmail.com>
2022-10-29 14:27:30 -0300

    29/10

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ function Home() {
       <div className="w-2/3">
         <span className="font-black text-green-500 text-3xl">¡Dale vida y sanidad a tus pies!</span>
         <br />
-        <span className="font-black text-green-500 text-3xl">La podología es la ciencia sanitaria que tiene por objeto el estudio de las enfermedades y alteraciones que afectan al pie.</span>
+        <span className="font-black text-green-500 text-3xl">La podología realizada por un podologo es la ciencia sanitaria que tiene por objeto el estudio de las enfermedades y alteraciones que afectan al pie.</span>
       </div>
     </div>
   )
