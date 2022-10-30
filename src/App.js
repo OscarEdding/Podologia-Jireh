408dc20dee48b54c57f6b3e0f37b82e96808dd85
OscarEdding <oscaredding2848@hotmail.com>
2022-10-29 22:32:27 -0300

    compare head

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import "./Tailwind.css"
 import React from 'react'
-import { Route, Routes } from "react-router-dom"
 
-import ROUTES from "./constants/routes"
 import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
+import Section from "./components/Section/Section"
 import Home from "./pages/Home"
 import Services from "./pages/Services"
 import Gallery from "./pages/Gallery"
+import Footer from './components/Footer/Footer'
 import WhatsAppFloating from "./components/WhatsApp Button/WhatsAppFloating"
 
 function PodologiaJireh() {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path={ROUTES.SERVICES} element={<Services />} />
-        <Route path={ROUTES.GALLERY} element={<Gallery />} />
-      </Routes>
+      <div>
+      <Section
+        dark={true}
+        id="home"
+      />
+      <Home />
+
+      <Section
+        dark={true}
+        id="services"
+      />
+      <Services />
+
+      <Section
+        dark={true}
+        id="gallery"
+      />
+      <Gallery />
+      </div>
       <WhatsAppFloating />
       < Footer />
     </div>
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -3,7 +3,7 @@ import React from "react"
 function Gallery() {
 
   return (
-    <div className="h-screen pt-16">
+    <div>
       <section className="overflow-hidden text-gray-700 ">
         <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
           <div className="flex flex-wrap -m-1 md:-m-2">
