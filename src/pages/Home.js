843f06a6c93ae5a606af74af9bd69e338e5ff13b
OscarEdding <oscaredding2848@hotmail.com>
2022-11-07 01:27:03 -0300

    fixed desfase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ function Home() {
   const customAnimationFadeLeft = keyframes`
   from {
     opacity: 0;
-    transform: translate3d(-50px, 0, 0);
+    transform: translate3d(-40px, 0, 0);
   }
 
   to {
@@ -21,7 +21,7 @@ function Home() {
   const customAnimationFadeRight = keyframes`
   from {
     opacity: 0;
-    transform: translate3d(50px, 0, 0);
+    transform: translate3d(40px, 0, 0);;
   }
 
   to {
@@ -34,19 +34,19 @@ function Home() {
     <div>
       <Carousel />
       <div className="w-full h-full  bg-fixed bg-cover bg-[url('https://podologia-jireh.s3.sa-east-1.amazonaws.com/bg-podology.jpg')]">
-        <div className="py-10 bg-gray-50/50">
+        <div className="py-10 px-10 bg-gray-50/50">
           <Reveal keyframes={customAnimationFadeLeft}>
             <div className="flex flex-col py-5 items-start">
-              <div className="flex flex-col px-10 md:w-2/4">
+              <div className="flex flex-col md:w-2/4">
                 <p className="font-extrabold text-3xl text-white">¿Que es la podología clínica?</p>
                 <hr className="w-40 md:w-60 h-1 bg-sky-600 my-2 rounded-full" />
-                <p className="font-semibold text-lg">Podología clínica es la ciencia que abarca el diagnóstico y tratamiento de las afecciones de los pies mediante diversas técnicas.</p>
+                <p className="font-semibold text-lg">Podología clínica es la ciencia que abarca el diagnóstico y tratamiento de las afecciones de los pie mediante diversas técnicas.</p>
               </div>
             </div>
           </Reveal>
           <Reveal keyframes={customAnimationFadeRight}>
             <div className="flex flex-col py-5 items-end">
-              <div className="flex flex-col px-10 md:w-2/4 md:justify-items-end">
+              <div className="flex flex-col md:w-2/4 md:justify-items-end">
                 <p className="font-extrabold text-3xl text-white">¿Para que nos sirve?</p>
                 <hr className="w-40 md:w-60 h-1 bg-sky-600 my-2 rounded-full" />
                 <ul className="list-disc font-semibold text-lg px-5">
