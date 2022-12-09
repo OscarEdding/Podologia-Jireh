59d46a49fb66b20540772436ecd9f41d326d4609
OscarEdding <oscaredding2848@hotmail.com>
2022-12-09 13:12:21 -0300

    Carousel desktop version

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -26,20 +26,20 @@ const Carousel = () => {
 
   const carousel = [
     {
-      img: "https://podologia-jireh.s3.sa-east-1.amazonaws.com/Dise%C3%B1o+sin+t%C3%ADtulo.png",
+      img: "https://podologia-jireh.s3.sa-east-1.amazonaws.com/Carousel_1.png",
       alt: "1",
       active: "active"
     },
     {
-      img: "https://podologia-jireh.s3.sa-east-1.amazonaws.com/Carousel/Carousel_2.jpg",
+      img: "https://podologia-jireh.s3.sa-east-1.amazonaws.com/Carousel_2.png",
       alt: "2"
     },
     {
-      img: "https://instagram.fscl27-1.fna.fbcdn.net/v/t51.2885-15/143654007_409017116993149_8920383359288300757_n.jpg?se=7&stp=dst-jpg_e35&_nc_ht=instagram.fscl27-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=C9tVl9k2nvkAX92gktT&tn=p8q4hE4A6cPTnmiE&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=MjQ5NzQ1NjMzMzY2NDk1OTI2Mw%3D%3D.2-ccb7-5&oh=00_AfD_tqH_SM-MUYJFziwOhVMuP_K5DCo_hiA_mhsF8Pn-cw&oe=638C6769&_nc_sid=30a2ef",
+      img: "https://podologia-jireh.s3.sa-east-1.amazonaws.com/Comparison_v1.png",
       alt: "3"
     },
     {
-      img: "https://instagram.fscl27-1.fna.fbcdn.net/v/t51.2885-15/144222104_843703099507799_3641601201736759394_n.jpg?se=7&stp=dst-jpg_e35&_nc_ht=instagram.fscl27-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=tthPZTUTp_kAX_3ktOF&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=MjQ5NzQ1OTc3ODE2MTY0NTQzNA%3D%3D.2-ccb7-5&oh=00_AfBibRyW9XqInXnKNO2T28XzomnsiZyMRLPWtFOR1idbRQ&oe=638C5292&_nc_sid=30a2ef",
+      img: "https://podologia-jireh.s3.sa-east-1.amazonaws.com/Comparison_v2.png",
       alt: "4"
     },
   ]
